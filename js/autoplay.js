8fe241fe84f8fb72efbc52519ff29868854bc5e7
jbtgrt <128562704+jbtgrt@users.noreply.github.com>
2023-06-23 07:15:59 +0800

    Add files via upload

---
+++ js/autoplay.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+              var owl = $('.owl-carousel');
+              owl.owlCarousel({
+                items: 1,
+                loop: true,
+                margin: 0,
+                delay: 1000,
+                autoplay: true,
+                autoplayTimeout: 2500,
+                autoplayHoverPause: true
+           });
+ })
---
+++ js/profile.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 40,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
